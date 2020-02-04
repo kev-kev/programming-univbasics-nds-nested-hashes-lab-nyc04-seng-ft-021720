a04b6a4a102cc746792285b7a106ab9a406694f7
Kevin Clark <kevin.clark93@gmail.com>
2020-02-04 19:21:13 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,8 +18,8 @@ def literal_aoh
   # held in the constants
   character_info = [DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
                     JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-                    PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-                    KATE_G = { name:  "Kate Gompert", occupation: "None" }
+                    PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
+                    KATE_G = { name:  "Kate Gompert", occupation: "None" },
                     BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }]
   return character_info
 end
